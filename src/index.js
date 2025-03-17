502ab1d19b1e926389ed2fa72647e7793e2a826e
fabdouarrahmane <afmtla@gmail.com>
2025-03-17 15:06:48 +0100

    jsdoc config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+/**
+ * Affiche "Hello World" dans la console.
+ * Ceci est un exemple simple de commentaire JSDoc.
+ *
+ * @example
+ * console.log("Hello World!");
+ */
 console.log("Hello World!");
